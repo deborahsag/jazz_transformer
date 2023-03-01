c2c33087f5666c76c6ff9e1a46491609a197ffac
deborahsag <deborahsag@gmail.com>
2023-03-01 13:43:10 -0300

    updated train.py to train on Google Cloud TPU.

--- train.py
+++ train.py
@@ -29,39 +29,41 @@ from model_aug import TransformerXL
 
 if __name__ == '__main__':
 
-  # load dictionary
-  # generated from build_vocab.py
-  vocab = pickle.load(open('pickles/remi_wstruct_vocab.pkl', 'rb'))
-  event2word, word2event = vocab.event2idx, vocab.idx2event
-  
-  
-  # load train data
-  # training_seqs_final.pkl : all songs' remi format
-  training_data_file = "data/training_seqs_struct_new_final.pkl"
-  print("loading training data from {}".format(training_data_file))
-  training_seqs = pickle.load( open(training_data_file, 'rb') )
+    with tpu_strategy.scope(): # creating the model in the TPUStrategy scope means we will train the model on the TPU
 
-  # show size of trqaining data
-  print("Training data count: {}".format(len(training_seqs)))
-
-  # show range of augmentation
-  if args.augment:
-      print("Range of pitch transposition in augmentation: {}".format(args.pitchaug_range))
+          # load dictionary
+          # generated from build_vocab.py
+          vocab = pickle.load(open('pickles/remi_wstruct_vocab.pkl', 'rb'))
+          event2word, word2event = vocab.event2idx, vocab.idx2event
 
 
-  # declare model
-  model = TransformerXL(
-      event2word=event2word, 
-      word2event=word2event,
-      checkpoint=None,
-      is_training=True,
-      training_seqs=training_seqs)
-  
-  # train
-  if args.augment:
-      model.train_augment(output_checkpoint_folder=args.ckpt_dir, pitchaug_range=args.pitchaug_range, logfile=args.log_file)
-  else:
-      model.train(output_checkpoint_folder=args.ckpt_dir, logfile=args.log_file)
-  
-  # close
-  model.close()
+          # load train data
+          # training_seqs_final.pkl : all songs' remi format
+          training_data_file = "data/training_seqs_struct_new_final.pkl"
+          print("loading training data from {}".format(training_data_file))
+          training_seqs = pickle.load( open(training_data_file, 'rb') )
+
+          # show size of trqaining data
+          print("Training data count: {}".format(len(training_seqs)))
+
+          # show range of augmentation
+          if args.augment:
+              print("Range of pitch transposition in augmentation: {}".format(args.pitchaug_range))
+
+
+          # declare model
+          model = TransformerXL(
+              event2word=event2word,
+              word2event=word2event,
+              checkpoint=None,
+              is_training=True,
+              training_seqs=training_seqs)
+
+          # train
+          if args.augment:
+              model.train_augment(output_checkpoint_folder=args.ckpt_dir, pitchaug_range=args.pitchaug_range, logfile=args.log_file)
+          else:
+              model.train(output_checkpoint_folder=args.ckpt_dir, logfile=args.log_file)
+
+          # close
+          model.close()
